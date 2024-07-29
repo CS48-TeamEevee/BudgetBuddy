251d30bc53c0c6a617902ed6f12ce04d56991006
Benson Cheng <bensonhpcheng@hotmail.com>
2024-07-29 15:38:45 -0400

    MongoDB connected, need schema

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
 const PORT = 3000;
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
+connectDB()
 
 
 const server = app.listen(PORT, () => {
