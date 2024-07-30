50448e684c49da56978111a90692b300b10b8fb9
Benson Cheng <bensonhpcheng@hotmail.com>
2024-07-30 17:25:32 -0400

    built out login and sign up page with styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignupForm from './SignupForm.js';
+import LoginForm from './LoginForm.js';
+import InitialSetup from './InitialSetup.js';
+// import LoginForm from './LoginForm'; 
 const App = () => {
-  return <div class ="title" style= './styles.css'>Budget Buddy</div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<SignupForm />} />
+        <Route path='/login' element={<LoginForm />} />
+        {/* <Route path='/setup' element={<InitialSetup />} />
+        <Route path='/report' element={<Report />} /> */}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -14,8 +14,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+   
     });
     console.log('MongoDB connected');
   } catch (err) {
