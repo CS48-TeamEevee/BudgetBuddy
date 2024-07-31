b862b727a20c33b3f30321a70774e68001774eb2
DanHudgens <danw.hudgens@gmail.com>
2024-07-30 17:24:52 -0700

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import SignupForm from './components/SignupForm.jsx';
+import LoginForm from './components/LoginForm.jsx';
+import InitialSetup from './components/InitialSetup.jsx';
+import Report from './components/InitialReport.jsx';
+
+
 const App = () => {
-  return <div>HELLO</div>;
+  return (
+    <>
+      <div>Hello</div>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<SignupForm />} />
+            <Route path='/login' element={<LoginForm />} />
+            <Route path='/setup' element={<InitialSetup />} />
+            <Route path='/report' element={<Report />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 };
 
 export default App;
