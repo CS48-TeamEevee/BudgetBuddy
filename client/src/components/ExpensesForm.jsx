f4835342a2ddfaa54cd363bc0498b2385ba91958
DanHudgens <danw.hudgens@gmail.com>
2024-07-30 17:42:05 -0700

    added monthly update.jsx

--- client/src/components/ExpensesForm.jsx
+++ client/src/components/ExpensesForm.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
+import React from 'react';
 
-// function ExpensesForm({ expenses, handleExpenseChange, expenseOptions, title }) {
-//     return (
-//         <div className="form-group">
-//             <label>{title}:</label>
-//             <select onChange={(e) => handleExpenseChange(e.target.value)}>
-//                 <option value="">Select an expense</option>
-//                 {expenseOptions.map((option) => (
-//                     <option key={option} value={option}>{option}</option>
-//                 ))}
-//             </select>
-//             {Object.keys(expenses).map((type) => (
-//                 <div key={type} className="form-group">
-//                     <label>{type}:</label>
-//                     <input
-//                         type="number"
-//                         value={expenses[type]}
-//                         onChange={(e) => handleExpenseChange(type, e.target.value)}
-//                     />
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
+function ExpensesForm({ expenses, handleExpenseChange, expenseOptions, title }) {
+    return (
+        <div className="form-group">
+            <label>{title}:</label>
+            <select onChange={(e) => handleExpenseChange(e.target.value)}>
+                <option value="">Select an expense</option>
+                {expenseOptions.map((option) => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+            {Object.keys(expenses).map((type) => (
+                <div key={type} className="form-group">
+                    <label>{type}:</label>
+                    <input
+                        type="number"
+                        value={expenses[type]}
+                        onChange={(e) => handleExpenseChange(type, e.target.value)}
+                    />
+                </div>
+            ))}
+        </div>
+    );
+}
 
-// export default ExpensesForm;
+export default ExpensesForm;
